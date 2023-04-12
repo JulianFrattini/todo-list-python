00c5a4039940d5ca7584d9ccfc2999e43b6f09ab
MajdShanan <majdmazen24@gmail.com>
2023-04-12 18:19:31 +0200

    Co-authored-by: Kasper Falk <falkendev@gmail.com>

--- backend/test/demo/test_email.py
+++ backend/test/demo/test_email.py
@@ -1,9 +1,13 @@
 import pytest
 import unittest.mock as mock
-
+from src.controllers.usercontroller import UserController
 from src.util.helpers import hasAttribute, ValidationHelper
 
-# tests for the hasAttribute method
-@pytest.mark.demo
-def test_hasAttribute_true():
-    assert True == True
+def test_get_email_from_db():
+    mockDatabase = mock.MagicMock()
+    email = 'majdmazen24@gmail.com'
+    mockDatabase.find.return_value = [{'email': email}]
+    sut = UserController(dao=mockDatabase)
+    validationResult = sut.get_user_by_email(email)
+    assert validationResult == { "email": email}
+    
