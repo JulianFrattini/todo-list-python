157eef3f3f01a1ab28d36927af4928b9be873882
Ben Björkman <bbjork88@gmail.com>
2023-05-01 20:15:22 +0200

    some changes

--- backend/test/lab_unit_1/test_assignment_3.py
+++ backend/test/lab_unit_1/test_assignment_3.py
@@ -1,17 +1,39 @@
 import pytest
-import unittest.mock as mock
 from unittest.mock import patch
 from src.util.dao import DAO
-
-def test_create():
-
-    # with patch('pymongo.MongoClient') as dao_init_mock:
-    #     dao_init_mock.side_effect = lambda self: print("HALLOJ")
-        # dao_init_mock.assert_called_once_with(dao)
+from unittest.mock import Mock, call
 
 
-        # mock av data
-        # callar init med mock
-        # callar create med mock
+@pytest.mark.parametrize(
+    "input_id",
+    [
+        (
+            20
+        ), # test basic functionality
+    ]
+)
+def test_create(input_id):
 
-    dao = DAO("test")
+    
+    mock_insert_one = Mock()
+    mock_insert_one.inserted_id = input_id
+
+    mock_collection = Mock()
+    mock_collection.insert_one.return_value = mock_insert_one
+    mock_collection.find_one.return_value = {"Test"}
+
+    mock_database = Mock()
+    mock_database.__getitem__ = Mock(return_value=mock_collection)
+
+    mock_database.list_collection_names.return_value = ['test']
+
+    mock_client = Mock()
+    mock_client.edutask = mock_database
+
+    with patch('pymongo.MongoClient', return_value=mock_client) as mongo_mock:
+        dao = DAO("test")
+
+
+    assert dao.create({"data" : "data!"}) == ["Test"]
+
+    assert mock_collection.find_one.call_args_list == [call({"_id": input_id})]
