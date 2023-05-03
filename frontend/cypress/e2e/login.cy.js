e4a5f8633330af4357a4ae162ccb75bed90687f1
Ben Björkman <bbjork88@gmail.com>
2023-05-03 16:44:55 +0200

    assignment 4 init

--- frontend/cypress/e2e/login.cy.js
+++ frontend/cypress/e2e/login.cy.js
@@ -4,27 +4,30 @@ describe('Logging into the system', () => {
   let name // name of the user (firstName + ' ' + lastName)
   let email // email of the user
 
-  before(function () {
-    // create a fabricated user from a fixture
-    cy.fixture('user.json')
-      .then((user) => {
-        cy.request({
-          method: 'POST',
-          url: 'http://localhost:5000/users/create',
-          form: true,
-          body: user
-        }).then((response) => {
-          uid = response.body._id.$oid
-          name = user.firstName + ' ' + user.lastName
-          email = user.email
-        })
-      })
-  })
+  
+  // before(function () {
+  //   // create a fabricated user from a fixture
+  //   cy.fixture('user.json')
+  //     .then((user) => {
+  //       cy.request({
+  //         method: 'POST',
+  //         url: 'http://localhost:5000/users/create',
+  //         form: true,
+  //         body: user
+  //       }).then((response) => {
+  //         uid = response.body._id.$oid
+  //         name = user.firstName + ' ' + user.lastName
+  //         email = user.email
+  //       })
+  //     })
+  // })
 
   beforeEach(function () {
     // enter the main main page
     cy.visit('http://localhost:3000')
   })
+
+  // it("")
 
   it('starting out on the landing screen', () => {
     // make sure the landing page contains a header with "login"
@@ -32,31 +35,31 @@ describe('Logging into the system', () => {
       .should('contain.text', 'Login')
   })
 
-  it('login to the system with an existing account', () => {
-    // detect a div which contains "Email Address", find the input and type (in a declarative way)
-    cy.contains('div', 'Email Address')
-      .find('input[type=text]')
-      .type(email)
-    // alternative, imperative way of detecting that input field
-    //cy.get('.inputwrapper #email')
-    //    .type(email)
+//   it('login to the system with an existing account', () => {
+//     // detect a div which contains "Email Address", find the input and type (in a declarative way)
+//     cy.contains('div', 'Email Address')
+//       .find('input[type=text]')
+//       .type(email)
+//     // alternative, imperative way of detecting that input field
+//     //cy.get('.inputwrapper #email')
+//     //    .type(email)
 
-    // submit the form on this page
-    cy.get('form')
-      .submit()
+//     // submit the form on this page
+//     cy.get('form')
+//       .submit()
 
-    // assert that the user is now logged in
-    cy.get('h1')
-      .should('contain.text', 'Your tasks, ' + name)
-  })
+//     // assert that the user is now logged in
+//     cy.get('h1')
+//       .should('contain.text', 'Your tasks, ' + name)
+//   })
 
-  after(function () {
-    // clean up by deleting the user from the database
-    cy.request({
-      method: 'DELETE',
-      url: `http://localhost:5000/users/${uid}`
-    }).then((response) => {
-      cy.log(response.body)
-    })
-  })
+//   after(function () {
+//     // clean up by deleting the user from the database
+//     cy.request({
+//       method: 'DELETE',
+//       url: `http://localhost:5000/users/${uid}`
+//     }).then((response) => {
+//       cy.log(response.body)
+//     })
+//   })
 })
