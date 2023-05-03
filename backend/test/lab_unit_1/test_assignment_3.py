1f87c03babe7f2447d18bac4904a6208ec5279ea
Ben Björkman <bbjork88@gmail.com>
2023-05-03 12:31:26 +0200

    assignment3 done

--- backend/test/lab_unit_1/test_assignment_3.py
+++ backend/test/lab_unit_1/test_assignment_3.py
@@ -1,39 +1,32 @@
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, Mock, call
 from src.util.dao import DAO
-from unittest.mock import Mock, call
 
 
-@pytest.mark.parametrize(
-    "input_id",
-    [
-        (
-            20
-        ), # test basic functionality
-    ]
-)
-def test_create(input_id):
+class TestCreate:
+    @pytest.fixture
+    def mock_collection(self):
+        return Mock()
 
-    
-    mock_insert_one = Mock()
-    mock_insert_one.inserted_id = input_id
+    @pytest.fixture
+    def sut(self, mock_collection):
+        mock_insert_one = Mock()
+        mock_insert_one.inserted_id = 20
 
-    mock_collection = Mock()
-    mock_collection.insert_one.return_value = mock_insert_one
-    mock_collection.find_one.return_value = {"Test"}
+        mock_collection.insert_one.return_value = mock_insert_one
+        mock_collection.find_one.return_value = {"Test"}
 
-    mock_database = Mock()
-    mock_database.__getitem__ = Mock(return_value=mock_collection)
+        mock_database = Mock()
+        mock_database.__getitem__ = Mock(return_value=mock_collection)
+        mock_database.list_collection_names.return_value = ['test']
 
-    mock_database.list_collection_names.return_value = ['test']
+        mock_client = Mock()
+        mock_client.edutask = mock_database
 
-    mock_client = Mock()
-    mock_client.edutask = mock_database
+        with patch('pymongo.MongoClient', return_value=mock_client) as mongo_mock:
+            dao = DAO("test")
+            return dao
 
-    with patch('pymongo.MongoClient', return_value=mock_client) as mongo_mock:
-        dao = DAO("test")
-
-
-    assert dao.create({"data" : "data!"}) == ["Test"]
-
-    assert mock_collection.find_one.call_args_list == [call({"_id": input_id})]
+    def test_create(self, sut, mock_collection):
+        assert sut.create({"data": "data!"}) == ["Test"]
+        assert mock_collection.find_one.call_args_list == [call({"_id": 20})]
