e21fcf4a40cc70b9e22f3ca7dcb2baa1be1b8177
Ben Björkman <bbjork88@gmail.com>
2023-05-05 14:48:08 +0200

    Started task 4

--- frontend/cypress/e2e/login.cy.js
+++ frontend/cypress/e2e/login.cy.js
@@ -1,38 +1,56 @@
+
 describe('Logging into the system', () => {
   // define variables that we need on multiple occasions
   let uid // user id
   let name // name of the user (firstName + ' ' + lastName)
   let email // email of the user
+  let counter = 1
 
-  
-  // before(function () {
-  //   // create a fabricated user from a fixture
-  //   cy.fixture('user.json')
-  //     .then((user) => {
-  //       cy.request({
-  //         method: 'POST',
-  //         url: 'http://localhost:5000/users/create',
-  //         form: true,
-  //         body: user
-  //       }).then((response) => {
-  //         uid = response.body._id.$oid
-  //         name = user.firstName + ' ' + user.lastName
-  //         email = user.email
-  //       })
-  //     })
-  // })
+  before(function () {
+    // create a fabricated user from a fixture
+    cy.fixture('user.json')
+      .then((user) => {
+        cy.request({
+          method: 'POST',
+          url: 'http://localhost:5000/users/create',
+          form: true,
+          body: user
+        }).then((response) => {
+          uid = response.body._id.$oid
+          name = user.firstName + ' ' + user.lastName
+          email = user.email
+        })
+      })
+  })
 
   beforeEach(function () {
     // enter the main main page
     cy.visit('http://localhost:3000')
+
+    // log in
+    cy.get("#email")
+      .type(email)
+    cy.get(".submit-form input[type=submit]")
+      .click()
+  
+    // make task
+    cy.get("#title")
+      .type("Task " + counter.toString())
+      counter++
+    cy.get(".submit-form input[type=submit]")
+      .click()
   })
 
-  // it("")
+  it("add todo to task1", () => {
+    cy.get(".container-element:nth-of-type(1)")
+      .click()
+    cy.get("popup")
+  })
 
-  it('starting out on the landing screen', () => {
-    // make sure the landing page contains a header with "login"
-    cy.get('h1')
-      .should('contain.text', 'Login')
+  it("add todo to task2", () => {
+    cy.get(".container-element:nth-of-type(2)")
+      .click()
+    cy.get("popup")
   })
 
 //   it('login to the system with an existing account', () => {
@@ -53,13 +71,13 @@ describe('Logging into the system', () => {
 //       .should('contain.text', 'Your tasks, ' + name)
 //   })
 
-//   after(function () {
-//     // clean up by deleting the user from the database
-//     cy.request({
-//       method: 'DELETE',
-//       url: `http://localhost:5000/users/${uid}`
-//     }).then((response) => {
-//       cy.log(response.body)
-//     })
-//   })
+  after(function () {
+    // clean up by deleting the user from the database
+    cy.request({
+      method: 'DELETE',
+      url: `http://localhost:5000/users/${uid}`
+    }).then((response) => {
+      cy.log(response.body)
+    })
+  })
 })
