9ff7be6e0459314d38180a575b49b40ecc6802d7
Dorrachai <93660087+Dorrachai@users.noreply.github.com>
2023-05-12 13:54:47 +0100

    Update test_usercontroller.py

--- backend/test/demo/test_usercontroller.py
+++ backend/test/demo/test_usercontroller.py
@@ -7,7 +7,6 @@ from src.util.dao import DAO
 mock_dao = Mock(spec=DAO)
 uc = UserController(dao=mock_dao)
 
-# Test if email of existing user is valid
 @pytest.mark.unit
 def test_valid_email_existing_user():
     email = 'test@example.com'
@@ -16,23 +15,14 @@ def test_valid_email_existing_user():
     result = uc.get_user_by_email(email)
     assert result == {'email': email, 'name': 'Test User'}
 
-#Test valid email of a non-existing user
 @pytest.mark.unit
-def get_user_by_email(self, email):
-        if not isinstance(email, str):
-            raise ValueError('Error: invalid email address')
+def test_valid_email_non_existing_user():
+    email = 'non_existing@example.com'
+    mock_dao.find.return_value = []
 
-        users = self.dao.find({'email': email})
-        if not users:
-            return None
-        elif len(users) == 1:
-            return users[0]
-        else:
-            print(f'Error: more than one user found with mail {email}')
-            return users[0]
+    result = uc.get_user_by_email(email)
+    assert result is None
 
-
-# Test case for an invalid email format
 @pytest.mark.unit
 def test_invalid_email_format():
     email = 'invalid_email_format'
@@ -40,7 +30,6 @@ def test_invalid_email_format():
     with pytest.raises(ValueError):
         uc.get_user_by_email(email)
 
-# Test case for an empty email string
 @pytest.mark.unit
 def test_empty_email():
     email = ''
@@ -48,7 +37,6 @@ def test_empty_email():
     with pytest.raises(ValueError):
         uc.get_user_by_email(email)
 
-# Test case for a non-string email input
 @pytest.mark.unit
 def test_non_string_email_input():
     email = 123
@@ -56,7 +44,6 @@ def test_non_string_email_input():
     with pytest.raises(ValueError):
         uc.get_user_by_email(email)
 
-# Test case for when the database is unavailable
 @pytest.mark.unit
 def test_database_unavailable():
     email = 'test@example.com'
@@ -68,7 +55,6 @@ def test_database_unavailable():
     # Reset the side effect
     mock_dao.find.side_effect = None
 
-# Test case for duplicate email addresses in the database
 @pytest.mark.unit
 def test_duplicate_email_addresses():
     email = 'test@example.com'
@@ -79,4 +65,3 @@ def test_duplicate_email_addresses():
 
     result = uc.get_user_by_email(email)
     assert result == {'email': email, 'name': 'Test User'}
-
