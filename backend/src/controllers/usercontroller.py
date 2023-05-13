6b318d3c23e6ebe464208899f992c3432910b61b
Basel-Aljazaeri <114690771+Basel-Aljazaeri@users.noreply.github.com>
2023-05-13 13:21:31 +0200

    modified

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -3,7 +3,7 @@ from src.util.dao import DAO
 
 import re
 ## emailValidator = re.compile(r'.*@.*')
-## modified emailValidiator
+# modified emailValidiator 
 emailValidator = re.compile(r'^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+$')
 
 
