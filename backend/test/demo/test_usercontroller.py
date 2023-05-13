24a3929bf9664e77effc0508eca43997f72189f2
iRazzak <hama.rose007@hotmail.com>
2023-05-13 15:41:22 +0200

    updated ;)

--- backend/test/demo/test_usercontroller.py
+++ backend/test/demo/test_usercontroller.py
@@ -2,29 +2,30 @@ import pytest
 import unittest.mock as mock
 from src.controllers.usercontroller import UserController
 
-@pytest.mark.task2
 
+@pytest.mark.task2
 def test_get_user_by_email_connection_success_valid_one_match():
     mocked_doa = mock.MagicMock()
-    mocked_doa.find.return_value = [{ "name":"alex", "email":"alex@gmail.com"}]
+    mocked_doa.find.return_value = [
+        {"name": "alex", "email": "alex@gmail.com"}]
     sut = UserController(dao=mocked_doa)
 
     result = sut.get_user_by_email("alex@gmail.com")
-    assert result == {"name":"alex","email": "alex@gmail.com"}
-    
-@pytest.mark.task2
+    assert result == {"name": "alex", "email": "alex@gmail.com"}
 
+
+@pytest.mark.task2
 def test_get_user_by_email_connection_success_valid_multiple_match():
     mocked_doa = mock.MagicMock()
-    mocked_doa.find.return_value = [{"name":"alex", "email":"alex@gmail.com"},{ "name":"chelsea", "email":"alex@gmail.com"}]
+    mocked_doa.find.return_value = [
+        {"name": "alex", "email": "alex@gmail.com"}, {"name": "chelsea", "email": "alex@gmail.com"}]
     sut = UserController(dao=mocked_doa)
 
     result = sut.get_user_by_email("alex@gmail.com")
-    assert result == {"name":"alex","email": "alex@gmail.com"}
-    
-    
-@pytest.mark.task2
+    assert result == {"name": "alex", "email": "alex@gmail.com"}
 
+
+@pytest.mark.task2
 def test_get_user_by_email_connection_success_valid_no_match():
     mocked_doa = mock.MagicMock()
     mocked_doa.find.return_value = []
@@ -32,19 +33,64 @@ def test_get_user_by_email_connection_success_valid_no_match():
 
     result = sut.get_user_by_email("alex@gmail.com")
     assert result == None
-    
-    
-    
+
+
 @pytest.mark.task2
-def test_get_user_by_email_connection_success_invalid_no_match():
+def test_get_user_by_email_connection_success_missing_localPart_no_match():
     mocked_doa = mock.MagicMock()
     mocked_doa.find.return_value = []
     sut = UserController(dao=mocked_doa)
 
     with pytest.raises(ValueError) as e:
-        sut.get_user_by_email("alex#gmailcom")
+        sut.get_user_by_email("@gmail.com")
 
     assert str(e.value) == "Error: invalid email address"
+
+
+@pytest.mark.task2
+def test_get_user_by_email_connection_success_missing_domain_no_match():
+    mocked_doa = mock.MagicMock()
+    mocked_doa.find.return_value = []
+    sut = UserController(dao=mocked_doa)
+
+    with pytest.raises(ValueError) as e:
+        sut.get_user_by_email("alex@.com")
+
+    assert str(e.value) == "Error: invalid email address"
+
+
+@pytest.mark.task2
+def test_get_user_by_email_connection_success_missing_host_no_match():
+    mocked_doa = mock.MagicMock()
+    mocked_doa.find.return_value = []
+    sut = UserController(dao=mocked_doa)
+
+    with pytest.raises(ValueError) as e:
+        sut.get_user_by_email("alex@gmail.")
+
+    assert str(e.value) == "Error: invalid email address"
+
+
+@pytest.mark.task2
+def test_get_user_by_email_connection_success_missing_dot_no_match():
+    mocked_doa = mock.MagicMock()
+    mocked_doa.find.return_value = []
+    sut = UserController(dao=mocked_doa)
+
+    with pytest.raises(ValueError) as e:
+        sut.get_user_by_email("alex@gmailcom")
+
+    assert str(e.value) == "Error: invalid email address"
+
+
+@pytest.mark.task2
+def test_get_user_by_email_connection_success_missing_atSighn_no_match():
+    mocked_doa = mock.MagicMock()
+    mocked_doa.find.return_value = []
+    sut = UserController(dao=mocked_doa)
+
+    with pytest.raises(ValueError) as e:
+        sut.get_user_by_email("alexgmail.com")
 
 
 @pytest.mark.task2
