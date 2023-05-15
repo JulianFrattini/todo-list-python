7c7b5cc96c8941a244dba859c949a4ee9b19965e
Andreas Nilsson <andreas.nilsson8905@gmail.com>
2023-05-15 09:24:35 +0200

    cypress click

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1920,  // Set the desired width of the viewport
+  viewportHeight: 1080,  // Set the desired height of the viewport
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- frontend/cypress/e2e/test_click.cy.js
+++ frontend/cypress/e2e/test_click.cy.js
@@ -1,26 +1,69 @@
 describe('GUI Testing', () => {
-  let user;
-  const taskName = 'testTask'
-  const todoName = 'testTodo'
-  before(async () => {
-    user = await cy.fixture('user.json');
-    const response = await cy.request({
-      method: 'POST',
-      url: 'http://localhost:5000/users/create',
-      form: true,
-      body: user,
-    });
-    await cy.visit('http://localhost:3000/')
-    await cy.get('#email').type(user.email)
-    await cy.get('input[type="submit"]').click()
-    await cy.get('#title').type(taskName)
-    await cy.get('form').submit()
+  let email;
+  let userId;
+  const taskName = 'testTask';
+  const todoName = 'testTodo';
+  const doneTodo = 'doneTodo';
+  const notDoneTodo = 'notDoneTodo';
+  const deleteTodo = 'deleteTodo';
 
-  })
+  before(() => {
+    cy.fixture('user.json').then(user => {
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:5000/users/create',
+        form: true,
+        body: user,
+      }).then(response => {
+        cy.visit('http://localhost:3000/').then(() => {
+          email = response.body.email;
+          userId = response.body._id.$oid;
+          cy.get('#email').type(response.body.email).then(() => {
+            cy.get('input[type="submit"]').click().then(() => {
+              cy.get('#title').type(taskName).then(() => {
+                cy.get('form').submit().then(() => {
+                  cy.contains('div', taskName).click().then(() => {
+                    cy.get('.inline-form').type(doneTodo).then(() => {
+                      cy.get('.inline-form').submit().then(() => {
+                        cy.get('.inline-form').type(deleteTodo).then(() => {
+                          cy.get('.inline-form').submit().then(() => {
+                            cy.get('.inline-form').type(notDoneTodo).then(() => {
+                              cy.get('.inline-form').submit().then(() => {
+                            cy.request({
+                              method: 'GET',
+                              url: `http://localhost:5000/tasks/ofuser/${userId}`,
+                            }).then((response) => {
+                              const todoId = response.body[0].todos[1]._id.$oid;
+                                const data = new URLSearchParams();
+                                data.append('data', `{'$set': {'done': true}}`);
+                                fetch(`http://localhost:5000/todos/byid/${todoId}`, {
+                                    method: 'put',
+                                    body: data,
+                                    headers: { 'Cache-Control': 'no-cache' }
+                                })
+                            });
+                          });
+                        });
+                      });
+                    });
+                  });
+                });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+  
+  
+  
 
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
-    cy.get('#email').type(user.email)
+    cy.get('#email').type(email)
     cy.get('input[type="submit"]').click()
     cy.contains('div', taskName).click()
 
@@ -41,7 +84,7 @@ describe('GUI Testing', () => {
       .should('be.disabled'); // check if the submit button is initially disabled
     cy.get('.inline-form input[type="submit"]').click({ force: true }); // try click the button anyways
     cy.get('.todo-item .editable')
-      .should('have.length', 2); //check that no extra todos have been added
+      .should('have.length', 5); //check that no extra todos have been added
 
     // cy.get(".inline-form").submit()
     // cy.get('.todo-item .editable')
@@ -51,14 +94,14 @@ describe('GUI Testing', () => {
   // R8UC2 - Test case 1
   it('Toggle an active todo item to done', () => {
 
-    cy.contains('.editable', todoName)
+    cy.contains('.editable', notDoneTodo)
       .parent('.todo-item')
       .find('span.checker') // find the span.checker element within the .todo-item element
       .click({ force: true })
       .wait(1500)
       .should('have.class', 'checked');
 
-    cy.contains('.editable', todoName)
+    cy.contains('.editable', notDoneTodo)
       .parent('.todo-item')
       .find('span.editable') // find the span.checker element within the .todo-item element
       .should('have.css', 'text-decoration')
@@ -68,14 +111,14 @@ describe('GUI Testing', () => {
 
   // R8UC2 - Test case 2
   it('Toggle a done todo item to active', () => {
-    cy.contains('.editable', todoName)
+    cy.contains('.editable', doneTodo)
       .parent('.todo-item')
       .find('span.checker')
       .click({ force: true })
       .wait(1500)
       .should('have.class', 'unchecked');
 
-    cy.contains('.editable', todoName)
+    cy.contains('.editable', doneTodo)
       .parent('.todo-item')
       .find('span.editable')
       .should('have.css', 'text-decoration')
@@ -86,27 +129,27 @@ describe('GUI Testing', () => {
   it('Delete an Todo item', () => {
 
     cy.get('.todo-item .editable')
-    .should('have.length', 2); //check that length is 2 before deletion
+    .should('have.length', 5); //check that length is 2 before deletion
 
-    cy.contains('.editable', todoName)
+    cy.contains('.editable', deleteTodo)
       .parent('.todo-item')
       .find('span.remover')
       .click({ force: true })
 
-    cy.contains('.editable', todoName)
+    cy.contains('.editable', deleteTodo)
     .should('not.exist')
 
     cy.get('.todo-item .editable')
-    .should('have.length', 1); //check that the todo have been deleted
+    .should('have.length', 4); //check that the todo have been deleted
 
   })
 
 
   after(async () => {
-    const response = await fetch(`http://localhost:5000/users/bymail/${user.email}`)
+    const response = await fetch(`http://localhost:5000/users/bymail/${email}`)
     const result = await response.json();
     const userId = result._id.$oid
-    const deleteUser = await cy.request({
+    await cy.request({
       method: 'DELETE',
       url: `http://localhost:5000/users/${userId}`
     })
