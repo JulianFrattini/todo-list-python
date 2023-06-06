77c0ba458fc56403e11fe8727cdd2e95b76cef1d
Dorrachai <93660087+Dorrachai@users.noreply.github.com>
2023-06-06 13:23:15 +0100

    adding test cases following 4step and cleaning up db

--- backend/test/demo/test_dao.py
+++ backend/test/demo/test_dao.py
@@ -29,28 +29,32 @@ json_validator = {
 }
 
 @pytest.fixture()
-@pytest.mark.integration
 def daoSetUp():
     with patch("src.util.dao.getValidator") as mock_get_validator:
         mock_get_validator.return_value = json_validator
         dao = DAO("test_unauthenticated")
+
+        # Yield the DAO to the test
         yield dao
-@pytest.mark.integration
+
+        # Clean up: delete all documents in the collection after each test
+        dao.collection.delete_many({})
+
 def test_create_valid_object(daoSetUp):
     valid_data = {"product_name": "ProductA", "price": 25.99, "categories": ["Electronics", "Gadgets"], "is_active": True}
     result = daoSetUp.create(valid_data)
     assert result is not None
-@pytest.mark.integration
+
 def test_create_missing_required_properties(daoSetUp):
     invalid_data = {"product_name": "ProductB", "categories": ["Electronics", "Gadgets"]}
     with pytest.raises(Exception):
         daoSetUp.create(invalid_data)
-@pytest.mark.integration
+
 def test_create_incorrect_data_types(daoSetUp):
     invalid_data = {"product_name": "ProductC", "price": "100.0", "categories": ["Electronics", "Gadgets"], "is_active": "True"}
     with pytest.raises(Exception):
         daoSetUp.create(invalid_data)
-@pytest.mark.integration
+
 def test_create_unique_ids(daoSetUp):
     data1 = {"product_name": "ProductD", "price": 45.0, "categories": ["Electronics", "Gadgets"], "is_active": True}
     data2 = {"product_name": "ProductE", "price": 55.0, "categories": ["Electronics", "Gadgets"], "is_active": False}
@@ -59,12 +63,21 @@ def test_create_unique_ids(daoSetUp):
     assert result1["_id"] is not None
     assert result2["_id"] is not None
     assert result1["_id"] != result2["_id"]
-@pytest.mark.integration
-def test_create_no_valid_db_connection(daoSetUp):
-    test_data = {"product_name": "ProductF", "price": 75.99, "categories": ["Electronics", "Gadgets"], "is_active": True}
-    with patch("src.util.dao.MongoClient") as mock_client:
-        mock_client.side_effect = Exception("Database connection error")
-        with pytest.raises(Exception, match="Database connection error"):
-            daoSetUp.create(test_data)
 
+# Exhaustive Testing: Create an object with maximum allowed properties
+def test_create_max_allowed_properties(daoSetUp):
+    max_data = {"product_name": "ProductM", "price": 999999999.99, "categories": ["Cat1", "Cat2", "Cat3", "Cat4", "Cat5"], "is_active": True}
+    result = daoSetUp.create(max_data)
+    assert result is not None
 
+# Boundary Value Analysis (BVA): Create an object with minimum required properties
+def test_create_min_required_properties(daoSetUp):
+    min_data = {"product_name": "ProductN", "price": 0.0, "categories": [], "is_active": False}
+    result = daoSetUp.create(min_data)
+    assert result is not None
+
+# Equivalence Partitioning (EP): Create an object with some typical properties
+def test_create_typical_properties(daoSetUp):
+    typical_data = {"product_name": "ProductO", "price": 99.99, "categories": ["Electronics"], "is_active": True}
+    result = daoSetUp.create(typical_data)
+    assert result is not None
