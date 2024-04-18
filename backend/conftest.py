0c911fb8034f95ead58602c297a1f05cdfdffc5d
Alex Lindqvist <allq22@student.bth.se>
2024-04-18 15:59:08 +0200

    Create session to roll back changes

--- backend/conftest.py
+++ backend/conftest.py
@@ -13,3 +13,17 @@ def mongodb():
     except Exception as e:
         raise Exception("Could not connect to MongoDB. Please make sure that MongoDB is running.") from e
     return client
+
+@pytest.fixture
+def rollback_session(mongodb):
+    '''
+    This fixture creates a session that will be rolled back after the test function.
+    Meaning that any changes made to the database during the test will be undone.
+    The fixture value must be provided as a session argument to the database operations.
+    '''
+    session = mongodb.start_session()
+    session.start_transaction()
+    try:
+        yield session
+    finally:
+        session.abort_transaction()
