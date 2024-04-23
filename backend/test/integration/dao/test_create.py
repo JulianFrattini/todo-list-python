5908617c0f56da317fce8929c274fea4a711b70c
JockeTS <jotn22@student.bth.se>
2024-04-23 10:24:38 +0200

    test collection, yield, 4 cases

--- backend/test/integration/dao/test_create.py
+++ backend/test/integration/dao/test_create.py
@@ -5,50 +5,81 @@ from unittest.mock import patch
 from src.util.dao import DAO
 
 class TestTodo:
-    """ * """
+    """ Test cases for creating todo documents. """
 
     @pytest.fixture
-    @patch('src.util.dao.getValidator', autospec=True)
-    def sut(self, mocked_validator):
+    def sut(self):
+        with patch('src.util.dao.getValidator', autospec=True) as mocked_validator:
 
-        mocked_validator.return_value = {
-            "$jsonSchema": {
-                "bsonType": "object",
-                "required": ["description"],
-                "properties": {
-                    "description": {
-                        "bsonType": "string",
-                        "description": "the description of a todo must be determined",
-                        "uniqueItems": True
-                    }, 
-                    "done": {
-                        "bsonType": "bool"
+            mocked_validator.return_value = {
+                "$jsonSchema": {
+                    "bsonType": "object",
+                    "required": ["description"],
+                    "properties": {
+                        "description": {
+                            "bsonType": "string",
+                            "description": "the description of a todo must be determined",
+                            "uniqueItems": True
+                        },
+                        "done": {
+                            "bsonType": "bool"
+                        }
                     }
                 }
             }
-        }
 
-        sut = DAO(collection_name="todo")
+            sut = DAO(collection_name="test_todo")
 
-        return sut
+            yield sut
 
-        # yield sut
-
-        # Do after yield
-        # sut.drop()
+            # Do after yield
+            sut.drop()
 
     @pytest.mark.integration
     def test_create_valid(self, sut):
+        """ Test that creating a valid todo returns the created MongoDB document. """
+
         todo_dict = {
             "description": "Testing",
         }
 
-        # with pytest.raises(Exception):
-            # sut.create(todo_dict)
-
         result = sut.create(todo_dict)
 
-        assert isinstance(result, object)
+        assert todo_dict["description"] == result["description"]
 
+    @pytest.mark.integration
+    def test_create_invalid_missing_property(self, sut):
+        """ Test that creating a todo without a required property raises a WriteError. """
 
+        todo_dict = {
+            "desc": "Testing",
+        }
+
+        with pytest.raises(Exception):
+            sut.create(todo_dict)
+
+    @pytest.mark.integration
+    def test_create_invalid_wrong_data_type(self, sut):
+        """ Test that creating a todo with the wrong data type for a property raises a WriteError. """
+
+        todo_dict = {
+            "description": 1,
+        }
+
+        with pytest.raises(Exception):
+            sut.create(todo_dict)
+
+    # FAILS
+    @pytest.mark.integration
+    def test_create_valid_not_unique(self, sut):
+        """ Test that creating two valid todo:s with the same descriptions raises a WriteError. """
+
+        todo_dict = {
+            "description": "Testing2",
+        }
+
+        sut.create(todo_dict)
+
+        with pytest.raises(Exception):
+            sut.create(todo_dict)
 
