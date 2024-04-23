b32dcc70d8723b13351e5819ef82082d0c9cd299
Alex Lindqvist <allq22@student.bth.se>
2024-04-23 22:15:27 +0200

    Set default backend url

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,7 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:3000',
+    env: {
+      BACKEND_URL: 'http://localhost:5000'
+    },
   },
 
   component: {
