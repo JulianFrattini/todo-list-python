404d1367b55ef59b651c98bb024349ce784442e6
Alex Lindqvist <allq22@student.bth.se>
2024-04-24 12:27:04 +0200

    Move env up a level

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,16 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    BACKEND_URL: 'http://localhost:5000'
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
-    env: {
-      BACKEND_URL: 'http://localhost:5000'
-    },
+    baseUrl: 'http://localhost:3000'
   },
-
   component: {
     devServer: {
       framework: "create-react-app",
