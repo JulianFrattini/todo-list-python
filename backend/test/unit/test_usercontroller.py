ed99d924c1a08b123dc29214ebb0e61a7e0d3cbb
Jonathan Friberg <115478048+jonathanfriberg@users.noreply.github.com>
2024-05-10 13:43:37 +0200

    Add files via upload

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -3,13 +3,10 @@ from src.controllers.usercontroller import UserController
 from src.util.dao import DAO
 from unittest.mock import Mock
 
-
-
-mock_dao = Mock(spec=DAO) #Creating a mock DAO object
-user_controller = UserController(dao=mock_dao) #Creating a UserController object with the mock DAO object
+mock_dao = Mock(spec=DAO) # Creating a mock DAO object
+user_controller = UserController(dao=mock_dao) # Creating a UserController object with the mock DAO object
 
 # Email variables to test
-
 valid_email = 'valid@email.com'          #valid email
 invalid_email = 'invalid@email.com'      #non existing email
 wrong_format_email = 'wrongformat'       #wrong format email
@@ -25,15 +22,23 @@ def test_valid_email():
 
 # Test if email doesn't exist
 def test_email_not_found():
-    
     mock_dao.find.return_value = []
 
-    result = user_controller.get_user_by_email(invalid_email)
-    assert result is None
+    with pytest.raises(IndexError):
+        user_controller.get_user_by_email(invalid_email)
+
+# Test if multiple emails are found, if so, return first user.
+@pytest.mark.unit
+def test_multiple_emails_found():
+    mock_dao.find.return_value = [{'email': valid_email, 'name': 'User 1'}, {'email': valid_email, 'name': 'User 2'}]
+
+    result = user_controller.get_user_by_email(valid_email)
+
+    assert result == {'email': valid_email, 'name': 'User 1'}
 
 # Test if email has wrong format
 @pytest.mark.unit
-def invalid_format():
+def test_invalid_format():
 
     with pytest.raises(ValueError):
         user_controller.get_user_by_email(wrong_format_email)
@@ -44,4 +49,4 @@ def test_empty_field():
 
     with pytest.raises(ValueError):
         user_controller.get_user_by_email(empty_email)
-#hej testing
+
