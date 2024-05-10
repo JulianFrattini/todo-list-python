57c238babb767da4ef3cf11774012bf70f7dfbfd
Robin Johannesson <robinjohannesson2@gmail.com>
2024-05-10 10:53:21 +0200

    Fixed Assignment 2, Exercise 2
    I switch using `with pytest.raises(Exception)` to test exceptions.
    
    I also changed the functions name for better understanding.
    
    Committed by Robin Johannesson.

--- backend/test/controllers/test_usercontroller.py
+++ backend/test/controllers/test_usercontroller.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import patch, MagicMock
 
-# $ pytest -m controllers
+# $ pytest -m unit
 
 # different systems under test
 from src.controllers.usercontroller import UserController
@@ -9,12 +9,12 @@ from src.controllers.usercontroller import UserController
 # Test cases:
 # 1. The email address is not valid
 # 2. The email address is valid, but causes an exception from the database
-# 3. The email address is valid and the user is not found
+# 3. The email address is valid, and the user is not found
 # 4. The email address is valid and the user is found
 # 5. The email address is valid and multiple users are found
 
 @pytest.mark.unit
-def test_1():
+def test_invalid_email():
     user = {'firstName': 'Jane', 'lastName': 'Doe', 'email': 'jane.doe'}
 
     mockedDAO = MagicMock()
@@ -22,29 +22,21 @@ def test_1():
 
     uc = UserController(dao=mockedDAO)
 
-    try:
+    with pytest.raises(ValueError):
         uc.get_user_by_email(email='jane.doe')
-    except ValueError:
-        pass
-    else:
-        assert False, 'Expected ValueError but none was raised.'
 
 @pytest.mark.unit
-def test_2():
+def test_valid_email_exception_from_db():
     mockedDAO = MagicMock()
     mockedDAO.find.side_effect = Exception('Something went wrong')
 
     uc = UserController(dao=mockedDAO)
 
-    try:
+    with pytest.raises(Exception):
         uc.get_user_by_email(email='not.present.user@example.com')
-    except Exception:
-        pass
-    else:
-        assert False, 'Expected Exception but none was raised.'
 
 @pytest.mark.unit
-def test_3():
+def test_valid_email_user_not_found():
     user = None
 
     mockedDAO = MagicMock()
@@ -54,7 +46,7 @@ def test_3():
     assert uc.get_user_by_email(email='janedoe@example.com') == None
 
 @pytest.mark.unit
-def test_4():
+def test_valid_email_user_found():
     user = {'firstName': 'Jane', 'lastName': 'Doe', 'email': 'janedoe@example.com'}
 
     mockedDAO = MagicMock()
@@ -64,12 +56,15 @@ def test_4():
     assert uc.get_user_by_email(email='janedoe@example.com') == user
 
 @pytest.mark.unit
-def test_5():
+def test_valid_email_multiple_users():
     user_a = {'firstName': 'Jane', 'lastName': 'Doe', 'email': 'janedoe@example.com'}
     user_b = {'firstName': 'John', 'lastName': 'Doe', 'email': 'janedoe@example.com'}
 
     mockedDAO = MagicMock()
     mockedDAO.find.return_value = [user_a, user_b]
 
-    uc = UserController(dao=mockedDAO)
-    assert uc.get_user_by_email(email='janedoe@example.com') == user_a
+    with patch('builtins.print') as mock_print:
+        uc = UserController(dao=mockedDAO)
+        assert uc.get_user_by_email(email='janedoe@example.com') == user_a
+
+        mock_print.assert_called_once_with(f'Error: more than one user found with mail janedoe@example.com')
