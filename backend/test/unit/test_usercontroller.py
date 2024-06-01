d7c4efc9b0b69c45303941b79add53b43feb2f7f
Cassandra <Cassofluhr@hotmail.com>
2024-06-01 16:59:14 +0200

    user updates

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -1,5 +1,6 @@
 import pytest
 import unittest.mock as mock
+import re
 
 from src.controllers.usercontroller import UserController
 
@@ -15,20 +16,19 @@ def test_valid_email_single_user(user_controller):
     result = user_controller.get_user_by_email('Rama@gmail.com')
     
     assert result == {'email': 'Rama@gmail.com', 'user': 'Rama'}
-    user_controller.dao.find.assert_called_once_with({'email': 'Rama@gmail.com'})
-    
 
 
 def test_valid_email_multiple_users(user_controller, capsys):
     user_controller.dao.find = mock.Mock(return_value=[{'email': 'Rama@gmail.com', 'user': 'Rama'}, {'email': 'Rama@gmail.com', 'user': 'Casso'}])
-    
+
     result = user_controller.get_user_by_email('Rama@gmail.com')
 
-    # Error THEN name 
-    captured = capsys.readouterr()
-    assert "Error: more than one user found with mail Rama@gmail.com" in captured.out
     assert result == {'email': 'Rama@gmail.com', 'user': 'Rama'}
+
     user_controller.dao.find.assert_called_once_with({'email': 'Rama@gmail.com'})
+
+    captured = capsys.readouterr()
+    assert captured.out == "Error: more than one user found with mail Rama@gmail.com\n"
 
 
 def test_valid_email_no_user(user_controller):
@@ -41,6 +41,7 @@ def test_valid_email_no_user(user_controller):
 def test_invalid_email(user_controller):
     with pytest.raises(ValueError) as exc_info:
         user_controller.get_user_by_email('uiauwfbiawubf')
+
     assert 'Error: invalid email address' in str(exc_info.value)
 
 
@@ -49,4 +50,5 @@ def test_database_exception(user_controller):
 
     with pytest.raises(Exception) as exc_info:
         user_controller.get_user_by_email('Rama@gmail.com')
+
     assert 'Database failure' in str(exc_info.value)
