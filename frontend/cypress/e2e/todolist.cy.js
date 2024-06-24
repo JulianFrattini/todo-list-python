7d31e7948384c0fb555898cc780d05196ad42fc0
Milad Rezai88 <96913621+Miladrezai13@users.noreply.github.com>
2024-06-24 17:42:43 +0200

    Update todolist.cy.js

--- frontend/cypress/e2e/todolist.cy.js
+++ frontend/cypress/e2e/todolist.cy.js
@@ -4,7 +4,6 @@ describe('GUI Testing', () =>
     let uid;
     let name;
     let taskTitle = 'GUI'; // Title for the task in tests
-    let todo1 = 'do gui test'; // Description for the first todo item
 
     before(function ()
     {
@@ -26,100 +25,68 @@ describe('GUI Testing', () =>
 
     beforeEach(function ()
     {
-        // Visit main page before each test
+        // Setup necessary state via backend before each test
+        cy.request('POST', 'http://localhost:5002/tasks/create', { userId: uid, title: taskTitle })
+            .then(response =>
+            {
+                cy.wrap(response.body).as('currentTask');
+            });
         cy.visit('http://localhost:3000');
     });
 
-    it('validates the login screen', () =>
+
+
+    it('creates a new todo-item via backend and confirms in GUI', () =>
     {
-        // Ensure the login screen is correctly displayed
-        cy.get('h1').should('contain.text', 'Login');
+        cy.get('@currentTask').then(task =>
+        {
+            cy.request('POST', `http://localhost:5002/tasks/${task._id}/todos/create`, { description: 'do gui test' });
+        });
+        cy.get('.todo-list').should('contain.text', 'do gui test');
     });
 
 
 
-    //----------------R8UC1--------------//
-
-    it(' creating a new todo-item', () =>
+    it('checks that the Add button remains disabled if the description is empty', () =>
     {
-        // Test the creation of a new todo item
-        cy.contains('div', 'Email Address').find('input[type=text]').type('mon.doe@gmail.com');
-        cy.get('form').submit();
-        cy.get('h1').should('contain.text', 'Your tasks, ' + name);
-
-        cy.get('.inputwrapper #title').type(taskTitle);
-        cy.get('form').submit();
-
-        cy.contains('div', taskTitle).should('contain.text', taskTitle).click();
-        cy.get('.popup-inner').find('form').should('have.class', 'inline-form')
-            .find('input[type="text"]').as('todoInput').should('exist').and('have.value', '');
-
-        cy.get('.popup-inner').find('.todo-list').find('input[value="Add"]').as('addButton')
-            .should('be.disabled').then(($btn) =>
-            {
-                cy.get('@todoInput').type(todo1, { force: true }).should('not.have.value', '');
-                cy.get('@addButton').should('be.enabled').click();
-            });
-
-        cy.get('.todo-list').children('.todo-item').last().should('contain.text', todo1);
+        cy.get('input[type="text"]').as('todoInput').clear();
+        cy.get('input[value="Add"]').should('be.disabled');
     });
 
 
-    //----------------R8UC2--------------//
-    it('toggles an active  to done', () =>
+
+
+    it('toggles a todo item from active to done via backend and confirms in GUI', () =>
     {
-        // Test the toggling of an active 'done' status
-        cy.contains('div', 'Email Address').find('input[type=text]').type('mon.doe@gmail.com{enter}');
-        cy.get('.container-element').find('a').first().click();
-
-        cy.get('.todo-list').first().find('.todo-item').first().as('todoItem')
-            .find('span.editable').as('todoName')
-            .should('not.have.css', 'text-decoration', 'line-through');
-
-        cy.get('@todoItem').find('span.checker').click();
-        cy.get('@todoName').should('have.css', 'text-decoration', 'line-through solid rgb(49, 46, 46)');
+        cy.get('@currentTask').then(task =>
+        {
+            cy.request('POST', `http://localhost:5002/tasks/${task._id}/todos/toggle`, { status: 'active' });
+        });
+        cy.get('.todo-item').find('span.checker').click();
+        cy.get('.todo-item').should('have.css', 'text-decoration', 'line-through solid rgb(49, 46, 46)');
     });
 
 
-    //----------------R8UC2--------------//
-    it('reverts a done to active', () =>
+
+
+    it('deletes a todo item via backend and confirms deletion in GUI', () =>
     {
-        // Test reverting done active status
-        cy.contains('div', 'Email Address').find('input[type=text]').type('mon.doe@gmail.com{enter}');
-        cy.get('.container-element').find('a').first().click();
-
-        cy.get('.todo-list').first().find('.todo-item').first().as('todoItem')
-            .find('span.editable').as('todoName')
-            .invoke('css', 'text-decoration').should('include', 'line-through');
-
-        cy.get('@todoItem').find('span.checker').click();
-        cy.get('@todoName').should('not.have.css', 'text-decoration', 'line-through');
-    });
-
-
-    //----------------R8UC3--------------//
-    it('deletes a todo item', () =>
-    {
-        // Test the deletion of a todo item
-        cy.contains('div', 'Email Address').find('input[type=text]').type('mon.doe@gmail.com{enter}');
-        cy.get('.container-element').find('a').first().click();
-
-        cy.get('ul').should('have.class', 'todo-list');
-        cy.get('.todo-list > li').first().find('.remover').click();
-        cy.wait(500); // Wait for 500 ms
+        cy.get('@currentTask').then(task =>
+        {
+            cy.request('DELETE', `http://localhost:5002/tasks/${task._id}/todos/delete`);
+        });
         cy.get('.todo-list > li').should('not.exist');
-
     });
+
+
+
 
     after(function ()
     {
-        //  delete the user created for the test
+        // Cleanup by deleting the user created for the test
         cy.request({
             method: 'DELETE',
             url: `http://localhost:5002/users/${uid}`
-        }).then((response) =>
-        {
-            cy.log(response.body);
         });
     });
 });
